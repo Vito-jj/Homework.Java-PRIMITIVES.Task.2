c5f6ae8a9041e9c89abacf47988b58263dacda51
Vito <vito_bjj@mail.ru>
2022-09-19 14:23:41 +0300

    Create the first version of the program

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
 
-    
+    public static void main(String[] args) {
+
+        int startingBalance = 100; // Начальный баланс счета
+        int replenishmentAmount = 1200; // Сумма пополнения счета
+
+        int totalBalanceWithoutBonus = startingBalance + replenishmentAmount; // Общий баланс до расчета бонуса
+
+        int receivedFunds;
+        if (totalBalanceWithoutBonus > 1000) {
+            receivedFunds = replenishmentAmount / 100 + totalBalanceWithoutBonus;
+        } else {
+            receivedFunds = totalBalanceWithoutBonus;
+        }
+
+        System.out.println(("Баланс вашего счета_") + (receivedFunds));
+    }
 }
